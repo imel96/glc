988c17e1bfd8ab1edce39736ecd4103bff0642d2
Melby Sjamsuddin <melbysjamsuddin@Quiksilver.local>
2016-04-15 11:34:02 +1000

    topcoder golang challenge
    https://www.topcoder.com/challenge-details/30045939/?type=develop

---
+++ glc3.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	dice "github.com/imel96/glc/dice"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/mf", mfHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func mfHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Fprintf(w, "Hello, %q", r.Form["r"])
+	fmt.Fprintf(w, "Bye, %q", dice.MinimumFaces([]string{"137", "364", "115", "724"}))
+}
