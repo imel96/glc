54d2b21aa0ffbbbd6be642cfc8457409643d8c5d
Melby Sjamsuddin <melbys@benon.com>
2016-05-25 16:09:58 +1000

    count shoppers

--- survey/survey.go
+++ survey/survey.go
@@ -1,5 +1,15 @@
 package survey
 
 func CountBigShoppers(N int, s []int) int {
-	return 0
+	buyer := N
+
+	for _, purchases := range s {
+		buyer -= (N - purchases)
+	}
+	if buyer < 0 {
+		return 0
+
+	} else {
+		return buyer
+	}
 }
